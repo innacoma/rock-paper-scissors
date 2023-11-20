bed58e43895501837f04268d5593569b6f573554
Coma <144150326+innacoma@users.noreply.github.com>
2023-11-19 23:48:36 -0500

    created computer choice

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+//Javascript page
 
+
+/*
+Create computer choice:
+1. create funtcion getComputerChoice
+2. make it randomly choose rock, paper, or scissors
+*/
+
+function getComputerChoice() {
+   let computerChoice = math.random();
+
+   if (computerChoice < 0.34) {
+        computerChoice = "rock";
+   }
+    elseif (computerChoice <= 0.67) {
+        computerChoice = "paper";
+    }
+    else {
+        computerChoice = "scissors";
+    }
+}

38ce14c2b30aa541f4cb666f06114d1d87b94124
Coma <144150326+innacoma@users.noreply.github.com>
2023-11-22 22:49:56 -0500

    changed from console play to buttons

--- script.js
+++ script.js
@@ -20,14 +20,17 @@ function getComputerChoice() {
     }
 
     return computerChoice;
+
 }
 
 //Gets player choice input
-function getPlayerChoice() {
+/* function getPlayerChoice() {
     playerChoice = prompt("Rock, Paper, or Scissors?:").toLowerCase();
 
     return playerChoice;
-}
+} */
+
+
 
 //Make outcome variables
 const loser = "Loser.";
@@ -35,30 +38,37 @@ const cheater = "Cheater.";
 const luck = "Lucky. It's a tie.";
 
 //Play the game
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+
+    const computerSelection = getComputerChoice();
+
+    let result = "";
+
     if (playerSelection == "paper" && computerSelection == "scissors" ||
         playerSelection == "scissors" && computerSelection == "rock" || 
         playerSelection == "rock" && computerSelection == "paper") {
-        return loser;
+        result = loser;
     }
     else if (playerSelection == "paper" && computerSelection == "rock" ||
             playerSelection == "scissors" && computerSelection == "paper" ||
             playerSelection == "rock" && computerSelection == "scissors") {
-        return cheater;
+        result = cheater;
     }
     else {
-        return luck;
+        result = luck;
     }
+
+    alert(result);
 }
 
 //Plays 5 games, then displays result of 5 game series
-function game() {
+/* function game() {
     let playerCount = 0; //sets player count to 0
     let computerCount = 0; //sets computer count to 0
 
     console.log("Hello!")
     for (let i = 0; i < 5; i++) { //starts i at and while it is less than 5 it will increase the count
-        const playerSelection = getPlayerChoice(); //assigns player choice function to a variable
+        //const playerSelection = getPlayerChoice(); //assigns player choice function to a variable
         const computerSelection = getComputerChoice(); //assigns computer choice function to a variable
         console.log(playRound(playerSelection, computerSelection)); //runs playRound function and displays result
         console.log("----------------") //separator
@@ -79,4 +89,4 @@ function game() {
     else {
         console.log("WTF How did you tie?!") //if tie 5 round score
     }
-}
+} */
